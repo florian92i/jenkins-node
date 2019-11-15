2a5ecb95f590274d63e4e2cbcb062ae69b326f72
Florian COMPIEGNE <florian.compiegne@eemi.com>
2019-11-15 10:48:31 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+
+app.set('port', process.env.PORT);
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Vous êtes à l\'accueil BBBBBBBRRRRRRRRRRRAAAAAAAAAAWWWWWWWWWWWWW');
+});
+
+app.listen(8081);
