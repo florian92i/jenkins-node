00bea18fab4035964b66079e28095fc1aed9b153
Florian Compiegne <florian.compiegne@eemi.com>
2020-03-06 17:02:17 +0100

    Update sum.test.js
    LOL

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,13 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+
+test('1 + 4 should not be equal to 3', () => {
+    expect(sum(1, 4)).NotToBe(3);
+});
+
+
+test('0 + 0 = tete a toto', () => {
+    expect(sum(0, 0)).toBe(toto);
+});
